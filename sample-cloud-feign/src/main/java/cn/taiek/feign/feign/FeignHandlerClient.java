4148c27b05113c1d0f284d102b8fd884eac80df5
huayadlly <huayadlly@gmail.com>
2017-09-23 11:14:45 +0800

    @FeignClient()；

--- sample-cloud-feign/src/main/java/cn/taiek/feign/feign/FeignHandlerClient.java
+++ sample-cloud-feign/src/main/java/cn/taiek/feign/feign/FeignHandlerClient.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by huayadlly on 2017/9/23.
  */
+
+/**
+ * 通过@FeignClient注解来绑定该接口对应的SAMPLE-CLOUD-PAPERINFO服务；
+ * 使用restTemplate的方式定义调用服务中对应的具体方法；
+ */
 @FeignClient("SAMPLE-CLOUD-PAPERINFO")
 public interface FeignHandlerClient {
 
